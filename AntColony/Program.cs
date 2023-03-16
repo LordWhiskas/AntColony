72dd99f375860f8ffa24bfb2de75e45519269d3d
Kyryl Zuiev <102185030+LordWhiskas@users.noreply.github.com>
2023-03-16 22:32:04 +0100

    Add files via upload

---
+++ AntColony/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntColony
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Path path = new Path(200, 1, 4, 0.36);
+            path.Execute();
+        }
+    }
+}
